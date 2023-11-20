eb952913221504142b31181e66977bf4211235d7
Ed59d <127413988+Ed59d@users.noreply.github.com>
2023-11-20 14:47:20 -0300

    v1
    trabalho de sergio

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br                                                                                                                                                                                                                                                  ">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>acessar</h1>
+    <form action="cadastro.php">
+        <p>
+            <label>Email</label>
+            <input type="Email"name="Email">
+        </p>
+        <p>
+        <label>senha</label>
+            <input type="password"name="senha">
+        </p>
+        <p>
+            <button type="submit">enviar</button>
+        </p>
+    </form>
+</body>
+</html>
